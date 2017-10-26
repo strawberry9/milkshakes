2329cbb8df209ed486d70d929b163998629b70aa
strawberry9 <me@noneofyourbusiness.com>
2017-10-26 23:10:40 +1100

    Updated Commit

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char *str = "Hello, world!";
-
 int main()
 {
+    const char *str = "Hello, world!";
+
     puts(str);
     
     return (EXIT_SUCCESS);
