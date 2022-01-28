89b040a6504cb44bb93106f85bbd1610c90b3b10
strawberry9 <strawberry9@users.noreply.github.com>
2022-01-28 13:02:47 +1100

    Create hello.d

---
+++ hello.d
@@ -0,0 +1,18 @@
+module test;
+@safe:
+
+import std;
+
+// https://dlang.org/spec
+// https://dlang.org/phobos/index.html
+// https://dlang.org/dstyle.html
+// https://github.com/dlang
+// https://wiki.dlang.org/Starting_as_a_Contributor
+// https://issues.dlang.org/
+// https://dlang.org/bugstats.html
+
+void main()
+{
+    writeln("Hello world!");
+
+}
