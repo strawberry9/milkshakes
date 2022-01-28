cd4473b801d1fb234f791c79347d4a1f87d12635
strawberry9 <strawberry9@users.noreply.github.com>
2022-01-28 13:25:52 +1100

    Update hello.d

--- hello.d
+++ hello.d
@@ -3,22 +3,26 @@ module test;
 
 import std;
 
-// https://forum.dlang.org/group/general
-
-// https://dlang.org/spec
-// https://dlang.org/phobos/index.html
-// https://dlang.org/dstyle.html
-
-// https://wiki.dlang.org/Coming_From
-// https://wiki.dlang.org/Get_involved
-// https://wiki.dlang.org/Starting_as_a_Contributor
-
-// https://issues.dlang.org/
-// https://dlang.org/bugstats.html
-// https://github.com/dlang
-
 void main()
 {
     writeln("Hello world!");
 
 }
+
+/+
+
+https://forum.dlang.org/group/general
+
+https://dlang.org/spec
+https://dlang.org/phobos/index.html
+https://dlang.org/dstyle.html
+
+https://wiki.dlang.org/Coming_From
+https://wiki.dlang.org/Get_involved
+https://wiki.dlang.org/Starting_as_a_Contributor
+
+https://issues.dlang.org/
+https://dlang.org/bugstats.html
+https://github.com/dlang
+
++/
