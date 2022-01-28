6ae5b094d0cd2844f3a96e9908600c760f04bb3d
strawberry9 <strawberry9@users.noreply.github.com>
2022-01-28 13:24:57 +1100

    Update hello.d

--- hello.d
+++ hello.d
@@ -3,13 +3,16 @@ module test;
 
 import std;
 
-// https://dlang.org/spec
-// https://dlang.org/phobos/index.html
 // https://forum.dlang.org/group/general
 
+// https://dlang.org/spec
+// https://dlang.org/phobos/index.html
 // https://dlang.org/dstyle.html
+
+// https://wiki.dlang.org/Coming_From
 // https://wiki.dlang.org/Get_involved
 // https://wiki.dlang.org/Starting_as_a_Contributor
+
 // https://issues.dlang.org/
 // https://dlang.org/bugstats.html
 // https://github.com/dlang
