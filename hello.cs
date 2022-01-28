bc3623d6abc1baffe10ab2b508197f97d8f2f5bf
strawberry9 <strawberry9@users.noreply.github.com>
2022-01-28 13:10:48 +1100

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static int Main()
+        {
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+}
